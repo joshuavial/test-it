7d17dd3c5bc79ded87c8b4a9c05c52ec38ec6100
Joshua Vial <joshua@enspiral.com>
2017-05-26 11:32:45 +1200

    example seeds

--- seeds/tests.js
+++ seeds/tests.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('tests').insert([
-        {id: 1, test_name: 'Ava', img_url: 'https://raw.githubusercontent.com/avajs/ava/master/media/header.png', description: '', installation: ''},
-        {id: 2, test_name: 'Enzyme', img_url: '', description: '', installation: ''},
+        {id: 1, test_name: 'Ava', img_url: 'https://raw.githubusercontent.com/avajs/ava/master/media/header.png', description: 'Ava is a test runner that runs your tests in parallel - see https://github.com/avajs/ava for more information.', installation: 'https://github.com/avajs/ava#usage'},
+        {id: 2, test_name: 'Enzyme', img_url: '', description: "A react testing utility that makes it easier to assert, manipulate, and traverse your React Components' output", installation: 'https://github.com/airbnb/enzyme#installation'},
         {id: 3, test_name: 'Tape', img_url: '', description: '', installation: ''},
         {id: 4, test_name: 'Supertest', img_url: '', description: '', installation: ''},
         {id: 5, test_name: 'Cheerio', img_url: '', description: '', installation: ''}
